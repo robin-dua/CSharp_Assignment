60eda181b3680f46676048e2bd531399e3a90765
Robin Dua <robin.dua@lngconsultancy.co.in>
2022-05-16 15:06:11 +0530

    Added Solution to Problem 2

---
+++ CSharp_Problem1/CSharp_Problem2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Table
+{
+    public static void Main()
+    {
+        int i, n;
+        Console.Write("Multiplication Table of 2 to 4:\n");
+
+        Console.Write("Enter the number: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        for (i = 1; i <= 4; i++)
+        {
+            Console.Write("{0} X {1} = {2} \n", n, i, n * i);
+        }
+    }
+}
