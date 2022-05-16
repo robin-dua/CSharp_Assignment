b601e0d3641a64fbff2a8404b021f2361f8d70f7
Robin Dua <robin.dua@lngconsultancy.co.in>
2022-05-16 15:20:32 +0530

    Added Solution to Problem 4

---
+++ CSharp_Problem1/CSharp_Problem4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Sum
+{
+    public static int SumCal(int n)
+    {
+        string n1 = Convert.ToString(n);
+        int sum = 0;
+        for (int i = 0; i < n1.Length; i++)
+            sum += Convert.ToInt32(n1.Substring(i, 1));
+        return sum;
+    }
+
+    public static void Main()
+    {
+        int num;
+        Console.Write("Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The sum of the digits of the number {0} is : {1} \n", num, SumCal(num));
+    }
+}
+
