2eb1a1ed053a0020dadd1fe6158db31de5b5c076
Robin Dua <103138471+robin-dua@users.noreply.github.com>
2022-05-16 19:07:54 +0530

    Changes Made

--- CSharp_Assessment/CSharp_Problem4/Program.cs
+++ CSharp_Assessment/CSharp_Problem4/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+/* Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+Test Data :
+Enter a number: 1234
+Expected Output :
+The sum of the digits of the number 1234 is : 10 */
+
+using System;
 public class Sum
 {
     public static int SumCal(int n)
