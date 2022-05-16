6f61f40515faaf0e8d44484bbe55f44fcd1472f5
Robin Dua <robin.dua@lngconsultancy.co.in>
2022-05-16 15:09:12 +0530

    Added Solution to Problem 3

---
+++ CSharp_Problem1/CSharp_Problem3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Pyramid
+{
+    public static void Main()
+    {
+        int i, j, space, rows, k;
+        Console.Write("Enter number of rows : ");
+        rows = Convert.ToInt32(Console.ReadLine());
+        space = rows + 4 - 1;
+        for (i = 1; i <= rows; i++)
+        {
+            for (k = space; k >= 1; k--)
+            {
+                Console.Write(" ");
+            }
+
+            for (j = 1; j <= i; j++)
+                Console.Write("* ");
+            Console.Write("\n");
+            space--;
+        }
+    }
+}
